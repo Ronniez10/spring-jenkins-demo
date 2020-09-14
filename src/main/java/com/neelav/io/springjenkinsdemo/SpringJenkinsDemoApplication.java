1634cccba2379ea350010551ae1576ff12315a5b
Neelav <bronnieneelav@gmail.com>
2020-09-14 21:52:35 +0530

    First Commit

---
+++ src/main/java/com/neelav/io/springjenkinsdemo/SpringJenkinsDemoApplication.java
@@ -0,0 +1,27 @@
+package com.neelav.io.springjenkinsdemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsDemoApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(SpringJenkinsDemoApplication.class);
+
+	@PostConstruct
+	public void init()
+	{
+		logger.info("Init is Called");
+	}
+
+	public static void main(String[] args)
+	{
+		logger.info("Application Executed");
+		SpringApplication.run(SpringJenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neelav/io/springjenkinsdemo/SpringJenkinsDemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.neelav.io.springjenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringJenkinsDemoApplicationTests {
+
+	private static Logger logger = LoggerFactory.getLogger(SpringJenkinsDemoApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Testcase Executing");
+		assertEquals(true,true);
+	}
+
+}
